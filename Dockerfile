af39369969a9f79501cad864a0848289245d1425
Guilherme Sampaio <guilhermefs09@protonmail.com>
2025-12-06 21:01:53 -0300

    update: cloudbuild

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM golang:1.22 AS builder
+
+WORKDIR /app
+
+COPY go.mod go.sum ./
+RUN go mod download
+
+COPY . .
+
+WORKDIR /app/cmd/api
+
+RUN go build -o /server .
+
+FROM gcr.io/distroless/base
+
+COPY --from=builder /server /server
+
+EXPOSE 8080
+
+CMD ["/server"]
--- internal/routes.go
+++ internal/routes.go
@@ -16,14 +16,14 @@ func InitRoutes(db *gorm.DB, e *echo.Echo) {
 	e.POST("/signup", authHandlers.SignupHandler)
 	e.POST("/login", authHandlers.LoginHandler)
 	e.POST("/refresh", authHandlers.RefreshHandler)
-	//e.POST("/v1/payments/webhook/:provider", paymentHandlers.WebhookHandler)
+	e.POST("/v1/payments/webhook/:provider", paymentHandlers.WebhookHandler)
 
 	fileHandlers := &filetools.ImageHandler{DB: db}
 	v1 := e.Group("/v1")
 	v1.Use(middlewares.AuthMiddleware())
 	v1.POST("/payments/create", paymentHandlers.CreatePayment)
 	v1.GET("/payments/:order_id/status", paymentHandlers.GetPaymentStatus)
-	//v1.GET("/payments/history", paymentHandlers.GetPaymentHistory)
+	v1.GET("/payments/history", paymentHandlers.GetPaymentHistory)
 	v1.POST("/image/upload", fileHandlers.UploadImage)
 	v1.GET("/image/status", fileHandlers.CheckStatusAsync)
 }
