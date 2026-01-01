68bc53043692309c70433b7d3b6b1c9184128bc0
Guilherme Sampaio <guilhermefs09@protonmail.com>
2026-01-01 19:24:57 -0300

    search: credit account restriction

--- internal/middlewares/jwt.go
+++ internal/middlewares/jwt.go
@@ -20,18 +20,28 @@ func AuthMiddleware() echo.MiddlewareFunc {
 func LoadUserMiddleware(db *gorm.DB) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
+
 			token, ok := c.Get("user").(*jwt.Token)
 			if !ok {
 				return echo.ErrUnauthorized
 			}
 
-			claims := token.Claims.(jwt.MapClaims)
-			userID := uint(claims["sub"].(float64))
+			claims, ok := token.Claims.(jwt.MapClaims)
+			if !ok {
+				return echo.ErrUnauthorized
+			}
 
+			uid, ok := claims["user_id"]
+			if !ok {
+				return echo.ErrUnauthorized
+			}
+
+			userID := uint(uid.(float64))
 			var user auth.User
 			if err := db.First(&user, userID).Error; err != nil {
 				return echo.ErrUnauthorized
 			}
+
 			c.Set("user", &user)
 			return next(c)
 		}
