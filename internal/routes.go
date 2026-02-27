83c37933f2afc03ea807fa60d215de98af0e7dd3
Guilherme Sampaio <g.sampaio@accenture.com>
2026-02-27 14:41:39 -0300

    update: endpoints

--- internal/routes.go
+++ internal/routes.go
@@ -14,11 +14,11 @@ func InitRoutes(db *gorm.DB, e *echo.Echo) {
 	authHandlers := &auth.AuthHandler{DB: db}
 	paymentHandlers := &payments.PaymentsHandler{DB: db}
 
-	e.POST("/signup", authHandlers.SignupHandler)
-	e.POST("/login", authHandlers.LoginHandler)
-	e.POST("/refresh", authHandlers.RefreshHandler)
+	e.POST("api/signup", authHandlers.SignupHandler)
+	e.POST("api//login", authHandlers.LoginHandler)
+	e.POST("api/refresh", authHandlers.RefreshHandler)
 
-	e.POST("/v1/payments/webhook/:provider", paymentHandlers.WebhookHandler)
+	e.POST("api/v1/payments/webhook/:provider", paymentHandlers.WebhookHandler)
 
 	fileHandlers := &filetools.ImageHandler{DB: db}
 	v1 := e.Group("/v1")
@@ -26,9 +26,9 @@ func InitRoutes(db *gorm.DB, e *echo.Echo) {
 		middlewares.AuthMiddleware(),
 		middlewares.LoadUserMiddleware(db),
 	)
-	v1.POST("/payments/create", paymentHandlers.CreatePayment)
-	v1.GET("/payments/:order_id/status", paymentHandlers.GetPaymentStatus)
-	v1.GET("/payments/history", paymentHandlers.GetPaymentHistory)
-	v1.POST("/image/upload", fileHandlers.UploadImage)
-	v1.GET("/image/status", fileHandlers.CheckStatusAsync)
+	v1.POST("api/payments/create", paymentHandlers.CreatePayment)
+	v1.GET("api/payments/:order_id/status", paymentHandlers.GetPaymentStatus)
+	v1.GET("api/payments/history", paymentHandlers.GetPaymentHistory)
+	v1.POST("api/image/upload", fileHandlers.UploadImage)
+	v1.GET("api/image/status", fileHandlers.CheckStatusAsync)
 }
